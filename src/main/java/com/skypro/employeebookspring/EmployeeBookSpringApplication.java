836c62abbf6a1cec2f9d298a64d6212ff8cfd915
letuu <albertvt@ya.ru>
2022-12-03 19:35:21 +0300

    Initial commit

---
+++ src/main/java/com/skypro/employeebookspring/EmployeeBookSpringApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.employeebookspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBookSpringApplication.class, args);
+    }
+
+}
